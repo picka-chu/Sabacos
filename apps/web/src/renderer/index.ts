00c6a0b8a4bcff8c0d137a44552183b4e8208c75
picka-chu <picka-chu@users.noreply.github.com>
2026-08-23 03:46:57 +0300

    Untrack unrelated motion-studio files (accidentally committed)

--- apps/web/src/renderer/index.ts
+++
@@ -1,6 +0,0 @@
-export { CompositionRenderer } from "./CompositionRenderer";
-export type { RendererOptions } from "./CompositionRenderer";
-export { createMediaResolver } from "./MediaCache";
-export type { MediaResolver, ResolvedMedia } from "./MediaCache";
-export { blendModeToPixi, rgbaToHex } from "./blend";
-export { makeChromaKeyFilter } from "./chromaKeyFilter";
